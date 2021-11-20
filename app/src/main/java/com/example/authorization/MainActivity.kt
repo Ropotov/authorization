67a7e9a18f87587b18e466759fe39621dec1022d
Nikita <ropotov.dev@gmail.com>
2021-11-20 20:13:31 +0400

    Добавлена колекция с логинами и паролями.

--- app/src/main/java/com/example/authorization/MainActivity.kt
+++ app/src/main/java/com/example/authorization/MainActivity.kt
@@ -5,10 +5,6 @@ import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-private const val adminLogin = "+7(999)999-99-99"
-private const val adminPass = "159357"
-
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,16 +14,27 @@ class MainActivity : AppCompatActivity() {
         btSingin.setOnClickListener {
             val login = etPhone.text.toString()
             val pass = etPassword.text.toString()
-            if (login == adminLogin && pass == adminPass) {
-                Toast.makeText(this, "Good Job", Toast.LENGTH_SHORT)
-                    .show()
+
+            if (dataMap.containsKey(login)) {
+
+                if (dataMap.getValue(login) == pass) {
+                    Toast.makeText(this, "Good Job", Toast.LENGTH_SHORT)
+                        .show()
+                } else {
+                    Toast.makeText(
+                        this, "Неверный логин или пароль", Toast.LENGTH_SHORT
+                    )
+                        .show()
+                }
             } else {
                 Toast.makeText(
-                    this, "Неверный логин или пароль", Toast.LENGTH_SHORT)
+                    this, "Неверный логин или пароль", Toast.LENGTH_SHORT
+                )
                     .show()
             }
         }
     }
 }
+
 
 
