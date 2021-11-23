8e043de48e03ec2e09b8ef948ecbefb95d33c90c
Nikita <ropotov.dev@gmail.com>
2021-11-23 19:33:48 +0400

    Переделан экран авторизации с использованием textInputLayout. Переписана функция для валидации Email.

--- app/src/main/java/com/example/authorization/RegActivity.kt
+++ app/src/main/java/com/example/authorization/RegActivity.kt
@@ -2,7 +2,6 @@ package com.example.authorization
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_reg.*
 
@@ -16,26 +15,31 @@ class RegActivity : AppCompatActivity() {
             val passReg = etPasswordReg.text.toString()
             val passRegRepeat = etPasswordReg2.text.toString()
 
-            if (!isLoginValid(loginReg)) {
-                showToast(this, "Неверный Email")
+
+            if (loginIsEmpty(loginReg)) {
+                showToast(this, "Email не заполнен")
             } else {
-                if (!isPassValid(passReg)) {
-                    showToast(this, "Пароль не соответствует требованиям")
-                    passClear()
+                if (!isLoginValid(loginReg)) {
+                    showToast(this, "Неверный Email")
                 } else {
-                    if (passReg != passRegRepeat) {
-                        showToast(this, "Пароли не совпадают")
+                    if (!isPassValid(passReg)) {
+                        showToast(this, "Пароль не соответствует требованиям")
                         passClear()
                     } else {
-                        if (dataMap.containsKey(loginReg)) {
-                            showToast(this, "Email уже зарегистрирован")
+                        if (passReg != passRegRepeat) {
+                            showToast(this, "Пароли не совпадают")
                             passClear()
                         } else {
-                            dataMap[loginReg] = passReg
-                            showToast(this, "Пользователь успешно арегистрирован")
-                            val intent = Intent(this, MainActivity::class.java)
-                            startActivity(intent)
+                            if (dataMap.containsKey(loginReg)) {
+                                showToast(this, "Email уже зарегистрирован")
+                                passClear()
+                            } else {
+                                dataMap[loginReg] = passReg
+                                showToast(this, "Пользователь успешно арегистрирован")
+                                val intent = Intent(this, MainActivity::class.java)
+                                startActivity(intent)
 
+                            }
                         }
                     }
                 }
--- app/src/main/java/com/example/authorization/methods.kt
+++ app/src/main/java/com/example/authorization/methods.kt
@@ -2,13 +2,27 @@ package com.example.authorization
 
 import android.content.Context
 import android.widget.Toast
+import java.util.regex.Pattern
+import kotlin.math.log
 
 fun showToast(context: Context, text: String) {
     Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
 }
 fun isLoginValid(login: String): Boolean {
-    return login.contains("@")
+    var isValid = false
+    val inputStr: CharSequence = login
+    val pattern = Pattern.compile(
+        "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$",
+        Pattern.CASE_INSENSITIVE
+    )
+    val matcher = pattern.matcher(inputStr)
+    if (matcher.matches()) isValid = true
+    return isValid
 }
+
 fun isPassValid(pass: String): Boolean{
     return pass.length in 8..16
 }
+
+fun loginIsEmpty(login: String) = login.isEmpty()
+
